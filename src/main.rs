76558f2c0a7d984a2d1c87572166ae7f68d8ea19
Pascual Soto Uribe <pascualsu@gmail.com>
2021-08-13 23:06:23 -0400

    New update 1628910383

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,8 @@ struct Attributes {
 async fn main() -> Result<(), Error> {
     let request_url = format!(
         "https://fintual.cl/api/goals?user_email=pascualsu%40gmail.com&user_token={fintual_token}",
-        fintual_token = env::var("FINTUAL_TOKEN").unwrap_or("none".to_string()));
+        fintual_token = env::var("FINTUAL_TOKEN").unwrap_or("none".to_string())
+    );
     let response = reqwest::get(&request_url).await?;
     let users: Data = response.json().await?;
 
@@ -32,18 +33,24 @@ async fn main() -> Result<(), Error> {
     println!("{:24} {:12} {:10} {:8}", "GOAL", "DEPOSITED", "BALANCE", "PROFIT");
     println!("{:-^56}", "-");
 
+    let mut deposited_acum: f64 = 0.0;
+    let mut balance_acum: f64 = 0.0;
+    let mut profit_acum: f64 = 0.0;
+
     for attr in users.data {
         let goal_name = attr.attributes.name;
         let deposited = attr.attributes.deposited;
         let balance = attr.attributes.nav;
         let profit = attr.attributes.profit;
 
-        println!("{:24} {:>9} {:>10.0} {:>9.0}", 
-            goal_name, 
-            deposited, 
-            balance, 
-            profit);
+        deposited_acum += deposited;
+        balance_acum += balance;
+        profit_acum += profit;
+
+        println!("{:24} {:>9} {:>10.0} {:>9.0}", goal_name, deposited, balance, profit);
     }
+
+    println!("{:24} {:>9} {:>10.0} {:>9.0}", "Total", deposited_acum, balance_acum, profit_acum);
     println!("{:-^56}", "-");
 
     Ok(())
